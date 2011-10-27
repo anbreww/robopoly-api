bc713dae49bbc97771c0c558c1ac26dc2d741b0b
Andrew Watson <andy@watsons.ch>
2011-10-27 23:34:30 +0200

    api: fix tabulation according to PEP 8

--- api.py
+++ api.py
@@ -8,24 +8,23 @@ app.debug = True
 
 @app.route("/")
 def hello():
-		return ".".join( [str(x) for x in sys.version_info] )
+    return ".".join( [str(x) for x in sys.version_info[0:3]] )
 
 @app.route("/people/<int:sciper>")
 def get_name(sciper):
-		# can't get ldap to compile with python 2.7, please don't hate me for this.
-		output = subprocess.Popen(["python", "/var/www/api/search.py",str(sciper)],stdout=subprocess.PIPE)
-		name = output.stdout.readlines()[0].rstrip()
-		return  name
+    # can't get ldap to compile with python 2.7, please don't hate me for this.
+    output = subprocess.Popen(["python", "/var/www/api/search.py",str(sciper)],stdout=subprocess.PIPE)
+    name = output.stdout.readlines()[0].rstrip()
+    return  name
 
 #Ohhhh ouiiii
 @app.route("/tsol/")
 @app.route("/tsol/<arret>/")
 @app.route("/tsol/<arret>/<sens>")
 def get_times(arret="EPFL", sens="R"):
-#		return tsol(arret, sens)
-		output = tsol.tsol(arret, sens)
-		return json.dumps(output)
+    output = tsol.tsol(arret, sens)
+    return json.dumps(output)
 
 if __name__ == "__main__":
-		app.debug = True
-		app.run()
+    app.debug = True
+    app.run(host='0.0.0.0')
